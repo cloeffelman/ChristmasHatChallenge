31323a4db2d18b9096ec6cad181d18ad849b2213
Cory Loeffelman <coryloeffelman@gmail.com>
2016-12-08 17:02:23 -0600

    Adding boilerplate project structure.

---
+++ client/components/Index/Index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class IndexComponent extends Component {
+  render() {
+    return (
+      <div>Hello</div>
+    );
+  }
+}
+
+export default IndexComponent;
