f73e427a667ef844b5dd422a0bea18dd7190b14e
Cory Loeffelman <Cloeffelman@shelterinsurance.com>
2017-11-16 10:30:36 -0600

    Snow is interactive.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Grid } from 'react-flexbox-grid';
+import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { Grid } from 'react-flexbox-grid'
 
 import Header from './header'
 
@@ -12,27 +12,59 @@ var Application = React.createClass({
     }
   },
   componentDidMount: function() {
-    this.setState({data: window.data});
+    this.setState({data: window.data})
+    particlesJS('snow', {
+      particles: {
+        number: {
+          value: 300
+        },
+        size: {
+          value: 5,
+          random: true,
+        },
+        opacity: {
+          random: true
+        },
+        move: {
+          direction: 'bottom'
+        },
+        line_linked: {
+          enable: false
+        }
+      },
+      interactivity: {
+        events: {
+          onhover: {
+            enable: true,
+            mode: 'repulse'
+          },
+          onclick: {
+            enable: true,
+            mode: 'bubble'
+          }
+        },
+        modes: {
+          repulse: {
+            distance: 100,
+            duration: 0.4
+          },
+          bubble: {
+            distance: 100,
+            size: 10
+          }
+        }
+      }
+    })
   },
   render: function() {
     return(
       <MuiThemeProvider>
         <div className = "content-wrapper">
-          <Snow></Snow>
+          <div id="snow"/>
           <Header contestants={this.state.data.contestants} title={this.state.data.title} totals={this.state.data.totals}/>
-          <Grid className = "grid">
-          </Grid>
+          <Grid className = "grid"/>
         </div>
       </MuiThemeProvider>
-    );
-  }
-});
-
-var Snow = React.createClass({
-  displayName: 'Header',
-  render: function() {
-    return(
-      <canvas id = "snow"></canvas>
     );
   }
 });
--- src/js/init.jsx
+++ src/js/init.jsx
@@ -1,19 +1,11 @@
-import 'babel-polyfill';
+import 'babel-polyfill'
 import Application from './app'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import snowObj from './effects/snow';
-require('../sass/app.scss');
-require('../sass/santa.scss');
-
-injectTapEventPlugin();
+require('../sass/app.scss')
+require('../sass/santa.scss')
 
 ReactDOM.render(
   <Application/>,
   document.getElementById('app')
-);
-
-$(document).ready(function() {
-  snowObj.init();
-});
+)
